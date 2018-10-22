8566accf69fb7bde0772b0dec77b4d4d2b9ad6f9
league <league@imac-9.attlocal.net>
2018-10-21 17:25:33 -0700

    I finished linear search, binary search, on interpolation search

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -11,18 +11,27 @@ class _04_SearchTest {
 	@Test
 	public void testLinearSearch() {
 		//1. use the assertEquals method to test your linear search method.
+		String words[] = {"Hi", "Hello", "Yeet", "G'Day", "Wasup"};
+		assertEquals(1, _00_LinearSearch.linearSearch(words, "Hello"));
+		assertEquals(2, _00_LinearSearch.linearSearch(words, "Yeet"));
+		assertEquals(0, _00_LinearSearch.linearSearch(words, "Hi"));
 	}
 
 	@Test
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
+		int array[] = {1, 2, 5, 7, 10, 15, 34};
+		assertEquals(2, _01_BinarySearch.binarySearch(array, 0, 6,  5));
+		assertEquals(6, _01_BinarySearch.binarySearch(array, 0, 6,  34));
+		assertEquals(3, _01_BinarySearch.binarySearch(array, 0, 6,  7));
 	}
 	
 	@Test
 	public void testInterpolationSearch() {
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
+		
 	}
 	
 	@Test
